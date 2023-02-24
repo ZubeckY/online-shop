e5366253b4dd4958d0ce16b95117eead7f596abf
Oleg Zubkov <77145335+ZubeckY@users.noreply.github.com>
2023-02-24 20:36:46 +0300

    Add files via upload

---
+++ pages/catalog/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-container>
+    <v-breadcrumbs :items="items" divider="-"></v-breadcrumbs>
+
+    <v-row>
+      <v-col cols="3">
+        <clean-card>
+          <v-card-title class="pt-2 pb-2">Фильтры</v-card-title>
+
+
+          <v-card-text class="pt-0 pb-0">Сортировать по</v-card-text>
+          <v-card-text class="pt-0">
+            <v-select :items="sortOne" hide-details
+                      outlined dense></v-select>
+          </v-card-text>
+
+          <v-card-text class="pt-0 pb-0">Сортировать по</v-card-text>
+          <v-card-text class="pt-0">
+            <v-select :items="sortTwo" hide-details
+                      outlined dense></v-select>
+          </v-card-text>
+
+
+          <v-card-text class="pt-0 pb-0">Цена</v-card-text>
+          <v-card-text class="pt-0">
+            <v-row>
+              <v-col class="px-4">
+                <v-range-slider v-model="range"
+                                hide-details
+                                class="align-center"
+                                :max="max" :min="min">
+                </v-range-slider>
+                <v-row>
+                  <v-col cols="auto">
+                    <v-text-field :value="range[0]"
+                                  class="mt-0 pt-0"
+                                  hide-details
+                                  outlined dense
+                                  type="number"
+                                  style="width: 90px"
+                                  @change="$set(range, 0, $event)"></v-text-field>
+                  </v-col>
+                  <v-spacer/>
+                  <v-col cols="auto">
+                    <v-text-field :value="range[1]"
+                                  class="mt-0 pt-0"
+                                  hide-details
+                                  outlined dense
+                                  type="number"
+                                  style="width: 90px"
+                                  @change="$set(range, 1, $event)"></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+
+        </clean-card>
+      </v-col>
+      <v-col cols="9">
+        <div class="d-flex flex-row flex-wrap">
+          <catalog-card v-for="i in 9" :key="i"></catalog-card>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator"
+
+@Component
+export default class Catalog extends Vue {
+  min:number = 100
+  max:number = 10000
+  range:number[] = [100, 10000]
+
+  sortOne = [
+    {
+      text: 'Название', value: 'title'
+    },
+    {
+      text: 'Цена', value: 'price'
+    }
+  ]
+  sortTwo = [
+    {
+      text: 'Возрастанию А-я', value: 'Asc'
+    },
+    {
+      text: 'Убыванию Я-а', value: 'Desc'
+    }
+  ]
+
+  items = [
+    {
+      text: 'Главная',
+      disabled: false,
+      href: '/',
+    },
+    {
+      text: 'Каталог',
+      disabled: false,
+      href: '/catalog',
+    }
+  ]
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <v-card color="black"
+            dark flat tile>
+      <v-window v-model="onboarding">
+        <v-window-item v-for="n in length"
+                       :key="`card-${n}`">
+          <v-card color="transparent" height="500">
+            <v-row class="fill-height"
+                   align="center" justify="center">
+              <v-card-text class="text-center">
+                Transparent themed, for background-imaged slides. Background color black added for demonstration purposes.
+              </v-card-text>
+            </v-row>
+          </v-card>
+        </v-window-item>
+      </v-window>
+
+      <v-card-actions class="justify-space-between">
+        <v-btn text @click="prev">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-item-group v-model="onboarding"
+                      class="text-center" mandatory>
+          <v-item v-for="n in length"
+                  :key="`btn-${n}`"
+                  v-slot="{ active, toggle }">
+            <v-btn :input-value="active"
+                   icon @click="toggle">
+              <v-icon>mdi-record</v-icon>
+            </v-btn>
+          </v-item>
+        </v-item-group>
+        <v-btn text @click="next">
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card elevation="0" rounded="0">
+      <v-container>
+        <v-row class="pt-3">
+          <v-col class="text-center">
+            <v-icon color="success" class="text--lighten-2" style="font-size: 45px">mdi-truck-fast-outline</v-icon>
+            <v-card-title class="justify-center pb-0" style="font-size: 16px">Бесплатная доставка</v-card-title>
+            <v-card-text style="font-size: 12px">Бесплатная доставка по России при сумме заказа от 3000 руб.</v-card-text>
+          </v-col>
+
+          <v-col class="text-center">
+            <v-icon color="success" class="text--lighten-2" style="font-size: 45px">mdi-clock-fast</v-icon>
+            <v-card-title class="justify-center pb-0" style="font-size: 16px">Поддержка 24/7</v-card-title>
+            <v-card-text style="font-size: 12px">На связи с клиентами 24 часа в сутки, 7 дней в неделю</v-card-text>
+          </v-col>
+
+          <v-col class="text-center">
+            <v-icon color="success" class="text--lighten-2" style="font-size: 45px">mdi-cart-arrow-up</v-icon>
+            <v-card-title class="justify-center pb-0" style="font-size: 16px">30 дней на возврат</v-card-title>
+            <v-card-text style="font-size: 12px">Простой возврат товара в течение 30 дней с даты покупки</v-card-text>
+          </v-col>
+
+          <v-col class="text-center">
+            <v-icon color="success" class="text--lighten-2" style="font-size: 45px">mdi-certificate-outline</v-icon>
+            <v-card-title class="justify-center pb-0" style="font-size: 16px">Гарантия качества</v-card-title>
+            <v-card-text style="font-size: 12px">Вся продукция имеет необходимые сертификаты</v-card-text>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+
+    <v-card elevation="0" rounded="0">
+
+    </v-card>
+
+
+    <v-form>
+      <v-container>
+        <v-card>
+
+          <v-card-title class="pb-0">Всё ещё здесь?</v-card-title>
+          <v-card-text>Подпишись на рассылку и будь вкурсе всех новинок на сайте</v-card-text>
+
+          <v-row class="ml-1">
+            <v-col cols="4">
+              <v-text-field dense outlined
+                            placeholder="Ваш email"/>
+            </v-col>
+            <v-col cols="2">
+              <v-btn color="success"
+                     class="text--lighten-2"
+                     style="text-transform: none">Отправить</v-btn>
+            </v-col>
+          </v-row>
+
+        </v-card>
+      </v-container>
+    </v-form>
+
+  </div>
+</template>
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator"
+@Component
+export default class Pages extends Vue {
+  length:number = 6
+  onboarding:number = 0
+
+  next () {
+    this.onboarding = this.onboarding + 1 === this.length ? 0 : this.onboarding + 1
+  }
+
+  prev () {
+    this.onboarding = this.onboarding - 1 < 0 ? this.length - 1 : this.onboarding - 1
+  }
+
+}
+</script>
